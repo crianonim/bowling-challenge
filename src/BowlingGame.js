bf58829a68a4f6497008a4871cf90164f3f9c85a
Jan Skowronski <crianonim@gmail.com>
2019-09-25 20:26:25 +0100

    first commit

--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -1,10 +1,30 @@
 var BowlingGame = function(){
   this.frameRolls = [];
+  this.currentFrame = [];
 };
 
 BowlingGame.prototype.roll = function (pins) {
   this.frameRolls.push(pins);
+  var isEven=this.frameRolls.length % 2 === 0
+  console.log("LENGTH after roll with pins:",pins," l: ", this.frameRolls.length)
+  var id="r"+this.frameRolls.length;
+  console.log({id})
+  document.getElementById(id).textContent=pins
+  var score=this.currentScore();
+  console.log({score})
+  document.getElementById("score").textContent=score;
+  if (pins===10 && !isEven){
+      this.roll(0);
+   }
 };
+
+BowlingGame.prototype.currentScore= function () {
+  var result=0;
+  for (var i=0;i<this.frameRolls.length;i++){
+    result+=this.frameRolls[i];
+  }
+  return result;
+}
 
 BowlingGame.prototype.score = function () {
   var result = 0;
@@ -46,3 +66,13 @@ BowlingGame.prototype.score = function () {
   }
 
 };
+var game=new BowlingGame();
+[5,4,10,4].forEach((el,i)=>setTimeout(()=>{
+  game.roll(el);
+},i*1000))
+// setTimeout(()=>{
+//   game.roll(5);
+//   game.roll(4);
+//   game.roll(7);
+//   game.roll(3);
+// },1000)
